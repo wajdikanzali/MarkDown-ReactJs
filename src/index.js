f55fc000f9f1282832ca273ad2e3c9fc208fc53e
Wajdi Kanzali <kanzali.wajdi@gmail.com>
2018-01-10 00:47:19 +0100

    create first component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// Ici on code notre App
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+    render(){
+        return (
+            <h1>Hello World !</h1>
+        )
+    }
+}
+
+render(
+    <App/>,
+    document.getElementById('root')
+);
