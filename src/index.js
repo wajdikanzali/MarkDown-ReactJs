314f4829b8ac2a42500a047aa8d9baa69f60ec53
wajdi kanzali <kanzali.wajdi@gmail.com>
2018-01-10 16:15:22 +0100

    Write HTML with JSX

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+//CSS
+import './style/css/bootstrap.min.css';
+import './index.css';
+
+//JS PERSO
+import { sampleText } from './sampleText';
+
+
 class App extends React.Component {
     render(){
         return (
-            <h1>Hello World !</h1>
+            <div className="container">
+            	<div className="row">
+
+            	 <div className="col-sm-6">
+            	  <textarea value={sampleText} rows="35" className="form-control">
+            	  	
+            	  </textarea>
+            	 </div>
+
+            	  <div className="col-sm-6">
+            	   <div>
+            	   	{sampleText}
+            	   </div>
+            	  </div>
+
+
+            	</div>
+            </div>
         )
     }
 }
